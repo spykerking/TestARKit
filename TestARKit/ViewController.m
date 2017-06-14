8f6a7408dc136b076bdf4986192fb5b5d512c0e4
suwei <suwei@hujiang.com>
2017-06-14 10:00:27 +0800

    create project

--- TestARKit/ViewController.m
+++ TestARKit/ViewController.m
@@ -8,13 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <ARSCNViewDelegate>
+@interface ViewController () <ARSCNViewDelegate,ARSessionDelegate>
 
 @property (nonatomic, strong) IBOutlet ARSCNView *sceneView;
 
+@property (nonatomic, strong) ARSession *arSession;
+
+@property (nonatomic, strong) ARWorldTrackingSessionConfiguration *arSessionConfiguration;
+
 @end
 
-    
 @implementation ViewController
 
 - (void)viewDidLoad {
@@ -27,20 +30,18 @@
     self.sceneView.showsStatistics = YES;
     
     // Create a new scene
-    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
+//    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
     
     // Set the scene to the view
-    self.sceneView.scene = scene;
+//    self.sceneView.scene = scene;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    // Create a session configuration
-    ARWorldTrackingSessionConfiguration *configuration = [ARWorldTrackingSessionConfiguration new];
+    self.sceneView.session = self.arSession;
     
-    // Run the view's session
-    [self.sceneView.session runWithConfiguration:configuration];
+    [self.sceneView.session runWithConfiguration:self.arSessionConfiguration];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -50,9 +51,21 @@
     [self.sceneView.session pause];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+- (ARSession *)arSession {
+    if (!_arSession) {
+        _arSession = [[ARSession alloc] init];
+        _arSession.delegate = self;
+    }
+    return _arSession;
+}
+
+- (ARWorldTrackingSessionConfiguration *)arSessionConfiguration {
+    if (!_arSessionConfiguration) {
+        _arSessionConfiguration = [[ARWorldTrackingSessionConfiguration alloc] init];
+        _arSessionConfiguration.lightEstimationEnabled = YES;
+        _arSessionConfiguration.planeDetection = ARPlaneDetectionHorizontal;
+    }
+    return _arSessionConfiguration;
 }
 
 #pragma mark - ARSCNViewDelegate
@@ -68,6 +81,32 @@
 }
 */
 
+- (void)renderer:(id <SCNSceneRenderer>)renderer
+      didAddNode:(SCNNode *)node
+       forAnchor:(ARAnchor *)anchor {
+    if ([anchor isMemberOfClass:[ARPlaneAnchor class]]) {
+        NSLog(@"捕捉到");
+        
+        ARPlaneAnchor *planeAnchor = (ARPlaneAnchor *)anchor;
+        
+        SCNBox *planeBox = [SCNBox boxWithWidth:planeAnchor.extent.x*0.2
+                                         height:0
+                                         length:planeAnchor.extent.x*0.2
+                                  chamferRadius:0];
+        
+        SCNNode *planeNode = [SCNNode nodeWithGeometry:planeBox];
+        planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z);
+        [node addChildNode:planeNode];
+        
+        //添加场景
+        SCNScene *scene = [SCNScene sceneNamed:@"air.scnassets/cup/cup.scn"];
+        
+        SCNNode *cupNode = scene.rootNode.childNodes[0];
+        cupNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z);
+        [node addChildNode:cupNode];
+    }
+}
+
 - (void)session:(ARSession *)session didFailWithError:(NSError *)error {
     // Present an error message to the user
     
@@ -81,6 +120,23 @@
 - (void)sessionInterruptionEnded:(ARSession *)session {
     // Reset tracking and/or remove existing anchors if consistent tracking is required
     
+}
+
+#pragma mark - ARSessionDelegate
+- (void)session:(ARSession *)session didUpdateFrame:(ARFrame *)frame {
+//    NSLog(@"相机移动");
+}
+
+- (void)session:(ARSession *)session didAddAnchors:(NSArray<ARAnchor*>*)anchors {
+    NSLog(@"添加锚点");
+}
+
+- (void)session:(ARSession *)session didUpdateAnchors:(NSArray<ARAnchor*>*)anchors {
+    NSLog(@"更新锚点");
+}
+
+- (void)session:(ARSession *)session didRemoveAnchors:(NSArray<ARAnchor*>*)anchors {
+    NSLog(@"移除锚点");
 }
 
 @end
